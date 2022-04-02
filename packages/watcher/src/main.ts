b6b42a4786ee4b36b7c49c074daec81cfc4657a7
Szymon Standarski <s.standarski@gmail.com>
2022-04-02 22:53:37 +0200

    restore old interval

--- packages/watcher/src/main.ts
+++ packages/watcher/src/main.ts
@@ -3,21 +3,21 @@ import { Gpio } from "onoff";
 const led = new Gpio(17, "out");
 const doorStatus = new Gpio(23, "in");
 
-// setInterval(() => {
-//   // doorStatus.readSync((err, value) => {
-//   //     led.writeSync(value);
-//   //     console.log(value)
-//   // })
+ setInterval(() => {
+   // doorStatus.readSync((err, value) => {
+   //     led.writeSync(value);
+   //     console.log(value)
+   // })
 
-//   const status = doorStatus.readSync();
-//   led.writeSync(status);
-// }, 100);
+   const status = doorStatus.readSync();
+   led.writeSync(status);
+ }, 100);
 
-doorStatus.watch((error, value) => {
-  console.table({ error, value });
+//doorStatus.watch((error, value) => {
+ // console.table({ error, value });
 
-  led.writeSync(value);
-});
+//  led.writeSync(value);
+//});
 
 process.on("SIGINT", (_) => {
   led.unexport();
