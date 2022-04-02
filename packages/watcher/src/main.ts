a603526c51200568c344dfc886852b7407514892
Szymon Standarski <s.standarski@gmail.com>
2022-04-02 23:21:35 +0200

    test with rxjs

--- packages/watcher/src/main.ts
+++ packages/watcher/src/main.ts
@@ -1,24 +1,24 @@
-// const Gpio = require('onoff').Gpio;
-import { Gpio } from "onoff";
+import { BinaryValue, Gpio, Low } from "onoff";
+import { BehaviorSubject, tap, debounceTime } from "rxjs";
 const led = new Gpio(17, "out");
 const doorStatus = new Gpio(23, "in");
 
- setInterval(() => {
-   // doorStatus.readSync((err, value) => {
-   //     led.writeSync(value);
-   //     console.log(value)
-   // })
+const doorAreOpenSubject = new BehaviorSubject<BinaryValue>(0);
 
-   const status = doorStatus.readSync();
-   led.writeSync(status);
- }, 100);
+//TODO add observable do któgo bedzie przekazywany strumień z informacją o statusie
+// następnie dodać pipa w którym będzie debouncer (spowoduje to zredukowanie szumów)
+// wysyłanie komunikatów dopiero gdy brama bedzie otwarta przez 5 s
 
-//doorStatus.watch((error, value) => {
- // console.table({ error, value });
+setInterval(() => {
+  const status = doorStatus.readSync();
+  console.warn(status);
+  doorAreOpenSubject.next(status);
+  // led.writeSync(status);
+}, 100);
 
-//  led.writeSync(value);
-//});
-
+doorAreOpenSubject
+  .pipe(tap(led.writeSync), tap(console.log), debounceTime(5000))
+  .subscribe((value) => console.warn(value));
 process.on("SIGINT", (_) => {
   led.unexport();
   doorStatus.unexport();
