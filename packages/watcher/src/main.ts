2f81c93f0af222955e135876006a2da0aae8726c
Szymon Standarski <s.standarski@gmail.com>
2022-05-02 07:16:25 +0200

    refactor - change streams

--- packages/watcher/src/main.ts
+++ packages/watcher/src/main.ts
@@ -1,29 +1,32 @@
 import { BinaryValue, Gpio } from "onoff";
-import { BehaviorSubject, distinctUntilChanged, tap } from "rxjs";
+import { distinctUntilChanged, tap, interval, map } from "rxjs";
+
 const led = new Gpio(17, "out");
 const doorStatus = new Gpio(23, "in");
 
 const CHECK_TIME_INTERVAL = 1000;
+const intervalSource = interval(CHECK_TIME_INTERVAL);
+let doorCurrentStatus: BinaryValue;
 
-const doorOpenedStatusSubject = new BehaviorSubject<BinaryValue>(0);
-
-const iv = setInterval(() => {
-  const status = doorStatus.readSync();
-  doorOpenedStatusSubject.next(status);
-}, CHECK_TIME_INTERVAL);
-
-const doorStream = doorOpenedStatusSubject
+const doorStream = intervalSource
   .pipe(
+    map<number, BinaryValue>((_) => {
+      return doorStatus.readSync();
+    }),
     distinctUntilChanged(),
+    tap((value) => {
+      doorCurrentStatus = value;
+    }),
     tap((value) => {
       led.writeSync(value);
     })
   )
   .subscribe((value) => console.log(`Door is ${value ? "open" : "closed"}`));
 
+  
+
 process.on("SIGINT", (_) => {
   led.unexport();
   doorStatus.unexport();
   doorStream?.unsubscribe();
-  clearInterval(iv);
 });
